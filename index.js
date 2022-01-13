3689543efa984847195a411a1fcc782f51f77856
Tomás R. Pita <tomasrpita@gmail.com>
2022-01-13 20:45:54 +0100

    Schema y types

---
+++ index.js
@@ -0,0 +1,14 @@
+'user strict';
+
+
+const {graphql, buildSchema} = require('graphql');
+
+// definiendo el esquema
+const schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`)
+
+// Ejecutar el query hello
+graphql(schema, '{ hello }').then(data => console.log(data));
