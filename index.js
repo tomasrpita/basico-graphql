65573855312ecd1f12468f41c929f8ac08748ece
Tomás R. Pita <tomasrpita@gmail.com>
2022-01-20 06:26:34 +0100

    mueve schema y resolvers a archivos independientes

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-'user strict';
+'user strict'
 
+const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
-const {graphql, buildSchema} = require('graphql');
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
-const schema = buildSchema(`
-	type Query {
-		"Retorna un saludo al mundo"
-		hello: String,
-		"Retorna un saludo a todos"
-		saludo: String
-	}
-`)
-
-// Configurar los resolvers
-const resolvers = {
-	hello: () => {
-		return 'Hola Mundo'
-	},
-	saludo: () => {
-		return 'Hola a Todos'
-	}
-}
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
 
 app.use('/api', graphqlHTTP({
-	schema: schema,
-	rootValue: resolvers,
-	graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-	console.log(`Server is listening at http://localhost:${port}/api`)
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
