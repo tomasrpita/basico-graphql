1aced86d692d32a42d28d2bf3195e527754ac574
Tomás R. Pita <tomasrpita@gmail.com>
2022-02-11 06:57:02 +0100

    Clase - Consumiendo el API

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
@@ -23,10 +25,12 @@ const schema = makeExecutableSchema({
   resolvers
 })
 
+app.use(cors())
+
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
